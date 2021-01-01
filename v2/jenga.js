f49edeee8ee61cca5946ec197c95805c48e1f518
ericschv@gmail.com <ericschv@gmail.com>
2021-01-01 14:02:01 -0500

    Refactor functions

--- v2/jenga.js
+++ v2/jenga.js
@@ -4,6 +4,63 @@ import * as THREE from "https://unpkg.com/three/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
 import { DragControls } from "https://unpkg.com/three/examples/jsm/controls/DragControls.js";
 
+const BOX_WIDTH = 1.5;
+const BOX_HEIGHT = 1;
+const BOX_DEPTH = 5;
+
+function createRectangle(scene, xRotation, yRotation, zRotation, xPosition, yPosition, zPosition) {
+    const geometry = new THREE.BoxGeometry(BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH);
+    const texture1 = new THREE.TextureLoader().load("textures/wood.jpg");
+    const texture2 = new THREE.TextureLoader().load("textures/wood_side.jpg");
+    const materials = [
+        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
+        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
+        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
+        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
+        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture2 }),
+        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture2 }),
+    ]
+    const rectangle = new THREE.Mesh(geometry, materials);
+    
+    rectangle.name = "rectangle";
+    rectangle.rotation.x = xRotation;
+    rectangle.rotation.y = yRotation;
+    rectangle.rotation.z = zRotation;
+    rectangle.position.x = xPosition;
+    rectangle.position.y = yPosition;
+    rectangle.position.z = zPosition;
+
+    scene.add(rectangle);
+
+    return rectangle;
+}
+
+function createJengaTower(scene, objects) {
+    for(let i = 1; i <= 18; i++) {
+        const yRotation = i % 2 === 0 ? 1.57 : 0;
+        const yPosition = i;
+        
+        for(let n = 1.75; n >= -1.75; n -= 1.75) {
+            const xPosition = i % 2 === 0 ? 0 : n;
+            const zPosition = i % 2 === 0 ? n : 0;
+            
+            objects.push(createRectangle(scene, 0, yRotation, 0, xPosition, yPosition, zPosition));
+        }
+    }
+}
+
+function createFloor(scene) {
+    const geometry = new THREE.PlaneGeometry(25, 25, 30);
+    const texture = new THREE.TextureLoader().load("textures/wood_table.jpg");
+    const material = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: texture });
+    const plane = new THREE.Mesh(geometry, material);
+
+    plane.rotation.x = 7.854;
+    plane.position.y = 0;
+
+    scene.add(plane);
+}
+
 function initCamera() {
     const fov = 50;
     const aspect = window.innerWidth / window.innerHeight
@@ -28,48 +85,6 @@ function initOrbitControls(renderer, camera) {
     }
 
     controls.update();
-}
-
-function createFloor(scene) {
-    const geometry = new THREE.PlaneGeometry(25, 25, 30);
-    const texture = new THREE.TextureLoader().load("textures/wood_table.jpg");
-    const material = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: texture });
-    const plane = new THREE.Mesh(geometry, material);
-
-    plane.rotation.x = 7.854;
-    plane.position.y = 0.49;
-
-    scene.add(plane);
-}
-
-function createRectangle(scene, xRotation, yRotation, zRotation, xPosition, yPosition, zPosition) {
-    const boxWidth = 1.5;
-    const boxHeight = 1;
-    const boxDepth = 5;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    const texture1 = new THREE.TextureLoader().load("textures/wood.jpg");
-    const texture2 = new THREE.TextureLoader().load("textures/wood_side.jpg");
-    const materials = [
-        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
-        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
-        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
-        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture1 }),
-        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture2 }),
-        new THREE.MeshPhongMaterial({ color: 0xDDDDDD, map: texture2 }),
-    ]
-    const rectangle = new THREE.Mesh(geometry, materials);
-    
-    rectangle.name = "rectangle";
-    rectangle.rotation.x = xRotation;
-    rectangle.rotation.y = yRotation;
-    rectangle.rotation.z = zRotation;
-    rectangle.position.x = xPosition;
-    rectangle.position.y = yPosition;
-    rectangle.position.z = zPosition;
-
-    scene.add(rectangle);
-
-    return rectangle;
 }
 
 function initDragControls(objects, camera, renderer) {
@@ -98,47 +113,34 @@ function initDragControls(objects, camera, renderer) {
     });
 }
 
-function initAnimation(renderer, camera, scene) {
-    function animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    };
-
-    animate();
-}
-
 function main() {
+    
     const renderer = new THREE.WebGLRenderer();
     const scene = new THREE.Scene();
     const skyLight = new THREE.HemisphereLight(0xFFFFFF, 0x080820, 1.2);
     const camera = initCamera();
-    const objects = [];
+    const jengaObjects = [];
 
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    };
+    
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-
+    
     scene.background = new THREE.Color(0xFFFFFF);
     scene.add(skyLight);
     
+    createJengaTower(scene, jengaObjects);
+    
     createFloor(scene);
     
-    for(let i = 1; i <= 18; i++) {
-        const yRotation = i % 2 === 0 ? 1.57 : 0;
-        const yPosition = i;
-    
-        for(let n = 1.75; n >= -1.75; n -= 1.75) {
-            const xPosition = i % 2 === 0 ? 0 : n;
-            const zPosition = i % 2 === 0 ? n : 0;
-            
-            objects.push(createRectangle(scene, 0, yRotation, 0, xPosition, yPosition, zPosition));
-        }
-    }
-
     initOrbitControls(renderer, camera);
-
-    initDragControls(objects, camera, renderer);
-
-    initAnimation(renderer, camera, scene);
+    
+    initDragControls(jengaObjects, camera, renderer);
+    
+    animate();
 }
     
 main();
